f5afa4e5438ac973e903285b65d76c8564da36df
Rosihan Ardiansyah <rosihan17@gmail.com>
2021-03-29 20:39:37 +0800

    delete unnecessary 2

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -9,11 +9,6 @@ class Account extends Model
         return $this->hasMany(User::class);
     }
 
-    public function organizations()
-    {
-        return $this->hasMany(Organization::class);
-    }
-
     public function docs()
     {
         return $this->hasMany(Docs::class);
