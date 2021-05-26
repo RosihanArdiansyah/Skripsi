a24d7def27fe0aba1c598acb6b9fce7f2328c8bf
Rosihan Ardiansyah <rosihan17@gmail.com>
2021-05-26 17:21:20 +0800

    user auth fix

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -12,7 +12,7 @@
         <div :class="isUrl('docs') ? 'text-white' : 'text-yellow-300 group-hover:text-white'">Documents</div>
       </inertia-link>
     </div>
-    <div v-if="'$page.props.auth.user.owner' === '1'">
+    <div v-if="$page.props.auth.user.owner == 1">
       <div class="mb-4">
         <inertia-link class="flex items-center group py-3" :href="route('users')">
           <icon name="users" class="w-4 h-4 mr-2" :class="isUrl('users') ? 'fill-white' : 'fill-yellow-400 group-hover:fill-white'" />
