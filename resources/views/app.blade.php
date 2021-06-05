3c875106c19305a636addfe94f8aaed7ed0f8b07
Rosihan Ardiansyah <rosihan17@gmail.com>
2021-06-05 11:41:46 +0800

    pwa bwta 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <html lang="en">
+    <meta name="description" content="Digital Library">
 
     {{-- Inertia --}}
     <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll,NodeList.prototype.forEach,Promise,Object.values,Object.assign" defer></script>
 
     {{-- Ping CRM --}}
     <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
+    <link rel=preconnect>
 
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src="{{ mix('/js/app.js') }}" defer>
+    </script>
     @routes
 </head>
 <body class="font-sans leading-none text-gray-700 antialiased">
