a38140a5a424e90e2e6f95cca739169cab4835d5
Rosihan Ardiansyah <rosihan17@gmail.com>
2021-08-21 00:49:40 +0800

    responsive user table

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -24,24 +24,29 @@
     <div class="bg-white rounded-md shadow overflow-x-auto">
       <table class="w-full whitespace-nowrap">
         <tr class="text-left font-bold">
-          <th class="px-6 pt-6 pb-4">Name</th>
-          <th class="px-6 pt-6 pb-4">Email</th>
-          <th class="px-6 pt-6 pb-4" colspan="2">Role</th>
+          <th class="px-6 pt-6 pb-4 font-bold uppercase bg-gray-200 text-gray-600 hidden md:table-cell">Nama</th>
+          <th class="px-6 pt-6 pb-4 font-bold uppercase bg-gray-200 text-gray-600 hidden md:table-cell">Email</th>
+          <th class="px-6 pt-6 pb-4 font-bold uppercase bg-gray-200 text-gray-600 hidden md:table-cell" colspan="2">Role</th>
         </tr>
-        <tr v-for="user in users" :key="user.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
-          <td class="border-t">
+        <tr v-for="user in users" :key="user.id" class="hover:bg-gray-100 focus-within:bg-gray-100
+        flex md:table-row flex-col md:flex-row border-4 md:border flex-wrap md:flex-no-wrap"
+        >
+          <td class="border-t block md:table-cell relative md:static">
+            <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Nama</span>
             <inertia-link class="px-6 py-4 flex items-center focus:text-indigo-500" :href="route('users.edit', user.id)">
               <img v-if="user.photo" class="block w-5 h-5 rounded-full mr-2 -my-2" :src="user.photo" />
               {{ user.name }}
               <icon v-if="user.deleted_at" name="trash" class="flex-shrink-0 w-3 h-3 fill-gray-400 ml-2" />
             </inertia-link>
           </td>
-          <td class="border-t">
+          <td class="border-t block md:table-cell relative md:static">
+            <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Email</span>
             <inertia-link class="px-6 py-4 flex items-center" :href="route('users.edit', user.id)" tabindex="-1">
               {{ user.email }}
             </inertia-link>
           </td>
-          <td class="border-t">
+          <td class="border-t block md:table-cell relative md:static">
+            <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Role</span>
             <inertia-link class="px-6 py-4 flex items-center" :href="route('users.edit', user.id)" tabindex="-1">
               {{ user.owner ? 'Owner' : 'User' }}
             </inertia-link>
