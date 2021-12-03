e6b74603691b5688d475a39a1bcd72dd050b1cbb
Rosihan Ardiansyah <rosihan17@gmail.com>
2021-12-03 16:11:48 +0800

    try 16

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -38,6 +38,7 @@ class DashboardController extends Controller
             'user' => Auth::user()->account->users()
             	->select('status',DB::raw('count(status) as online'))
                 ->where('status',1)
+                ->groupBy('status')
                 ->get()
                 ->transform(function ($docs) {
                     return [
