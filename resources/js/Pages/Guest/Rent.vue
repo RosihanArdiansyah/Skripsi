f1c62fb1460a8ca8becdb8e9f2fc252072f76d43
Rosihan Ardiansyah <rosihan17@gmail.com>
2022-03-03 17:25:32 +0800

    prosedur peminjaman

--- resources/js/Pages/Guest/Rent.vue
+++ resources/js/Pages/Guest/Rent.vue
@@ -2,7 +2,11 @@
   <div>
     <h1 class="mb-8 font-bold text-3xl">Prosedur Peminjaman</h1>
     <div class="mb-6 flex justify-between items-center">
-      <span>1.  </span>
+      <span>1. Mendaftar sebagai anggota perpustakaan <br />
+        <br />2. Memilih buku yang akan dipinjam<br />
+        <br />3. Menyerahkan buku yang akan dipinjam kepada pegawai perpustakaan<br />
+        <br />4. Pegawai perpustakaan akan mencatat nama buku dan waktu peminjaman pada buku peminjaman 
+        dan kartu anggota perpustakaan </span>
     </div>
   </div>
 </template>
